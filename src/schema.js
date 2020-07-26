d43512d95127c6f0598b4af49c93bcb5786631ce
kko0831 <kko0831@hanmail.net>
2020-07-26 13:06:29 +0900

    #1.2 Setting Up the Server like the Pros

---
+++ src/schema.js
@@ -0,0 +1,14 @@
+// api에 있는 파일들을 여기에서 합침
+import path from "path";
+import { makeExecutableSchema } from "graphql-tools";
+import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
+const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
+
+const schema = makeExecutableSchema({
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers),
+});
+
+export default schema;
--- src/server.js
+++ src/server.js
@@ -1,23 +1,29 @@
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
 //typeDefs
-const typeDefs = `
-    type Query{
-        hello : String!
-    }
-`;
+// const typeDefs = `
+// type Query{
+//   hello : String!
+// }
+// `;
 
 //resolvers
-const resolvers = {
-  Query: {
-    hello: () => "hello",
-  },
-};
+// const resolvers = {
+// Query: {
+// hello: () => "hello",
+// },
+// };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+// type , resolvers를 server.js에서 선언한 버전
+// const server = new GraphQLServer({typeDefs,resolvers});
+// schema가 api를 합친 버전
+const server = new GraphQLServer({ schema });
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
   console.log(`server is running http://localhost:${PORT}`)
