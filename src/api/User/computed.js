0ced8b7febce4f0d805adff08f5ccfc0be2891de
kko0831 <kko0831@hanmail.net>
2020-07-31 18:21:25 +0900

    #3.16 itsMe and amIFollowing Fileds part Two

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -7,28 +7,27 @@ export default {
       console.log(parent);
       return `${parent.firstName} ${parent.lastName}`;
     },
-    amIFollowing: async (parent, _, { request }) => {
+    isFollowing: async (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       try {
-        const existing = await prisma.$exists.user({
+        return prisma.$exists.user({
           AND: [
-            { id: parentId }, // 팔로잉하려는 사람의 ID가 존재하면서
-            { followers_some: [user.id] } // 그사람의 팔로워에 내가 있는가?
+            {
+              id: user.id
+            }, // 팔로잉하려는 사람의 ID가 존재하면서
+            {
+              following_some: {
+                id: parentId
+              }
+            } // 그사람의 팔로워에 내가 있는가?
           ]
         });
-        console.log(existing);
-        if (existing) {
-          return true;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.log(error);
+      } catch {
         return false;
       }
     },
-    itsMe: (parent, _, { request }) => {
+    isSelf: (parent, _, { request }) => {
       //check me
       const { user } = request; //현재 로그인한 유저 정보
       const { id: parentId } = parent; // User를 호출한 Query
