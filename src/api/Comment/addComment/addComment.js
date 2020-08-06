1eaae2d01f03e5654a8170f3fcbc6e0b9efa2205
kko0831 <kko0831@hanmail.net>
2020-08-07 03:14:55 +0900

    Minifixes

--- src/api/Comment/addComment/addComment.js
+++ src/api/Comment/addComment/addComment.js
@@ -1,5 +1,5 @@
-import { isAuthenticated } from '../../../middlewares';
-import { prisma } from '../../../../generated/prisma-client'; // 다른 폴더에서 열려있으면 import 됨
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client"; // 다른 폴더에서 열려있으면 import 됨
 
 export default {
   Mutation: {
@@ -7,7 +7,7 @@ export default {
       isAuthenticated(request); // 인증을 위해 필요함
       const { text, postId } = args; // 입력한 arguments
       const { user } = request; //request에 있는 user정보
-      const comment = await prisma.createComment({
+      return prisma.createComment({
         user: {
           connect: {
             id: user.id // request.user.id를 갖고있는 user를 연결해준다는 뜻인 것 같음
@@ -20,7 +20,6 @@ export default {
         },
         text // text도 넣어줌
       });
-      return comment;
     }
   }
 };
--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -2,9 +2,10 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    seeFullPost: async (_, args) => {
+    seeFullPost: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
       const { id } = args;
-      return prisma.post({ id });
+      return await prisma.post({ id });
     }
   }
 };
