dcb3becdda205b6a6e0533a6cf7e1592a05da9c0
kko0831 <kko0831@hanmail.net>
2020-08-07 23:38:53 +0900

    add postCount,commentCount

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -41,6 +41,11 @@ export default {
     likes: ({ id }) => prisma.user({ id }).likes(),
     comments: ({ id }) => prisma.user({ id }).comments(),
     rooms: ({ id }) => prisma.user({ id }).rooms(),
+    postsCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { user: { id } } })
+        .aggregate()
+        .count(),
     followingCount: ({ id }) =>
       prisma
         .usersConnection({ where: { followers_some: { id } } })
@@ -48,7 +53,7 @@ export default {
         .count(),
     followersCount: ({ id }) =>
       prisma
-        .usersConnection({ where: { following_none: { id } } })
+        .usersConnection({ where: { following_some: { id } } })
         .aggregate()
         .count()
   }
