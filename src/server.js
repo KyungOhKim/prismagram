09e295d65339092ea8a9842672af849b483f8c0c
kko0831 <kko0831@hanmail.net>
2020-09-22 19:45:19 +0900

    Preparing for Upload

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-import './env';
-import { GraphQLServer } from 'graphql-yoga';
-import logger from 'morgan';
-import schema from './schema';
-import './passport';
-import { isAuthenticated } from './middlewares';
-import { authenticateJwt } from './passport';
+import "./env";
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+import "./passport";
+import { isAuthenticated } from "./middlewares";
+import { authenticateJwt } from "./passport";
+import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,8 +13,9 @@ const server = new GraphQLServer({
   schema,
   context: ({ request }) => ({ request, isAuthenticated })
 });
-server.express.use(logger('dev'));
+server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
   console.log(`server is running http://localhost:${PORT}`)
